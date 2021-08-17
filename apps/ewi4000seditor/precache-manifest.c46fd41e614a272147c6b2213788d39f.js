51dca4c19d5d74abe319102f93210a168c089104
Ryoya Kawai <ryoya.kawai@gmail.com>
2021-08-17 22:20:37 +0900

    support pwa

---
+++ apps/ewi4000seditor/precache-manifest.c46fd41e614a272147c6b2213788d39f.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed487dc7ed4f46bae04",
+    "url": "css/app.36020867.css"
+  },
+  {
+    "revision": "df98d6f3521336f89215",
+    "url": "css/chunk-vendors.1a695c47.css"
+  },
+  {
+    "revision": "dca34ef9727ab3cb3bf403f7b6104de0",
+    "url": "fonts/DSEG-LICENSE.txt"
+  },
+  {
+    "revision": "586cd5496ac9e8d62e5a6d1a31f0588f",
+    "url": "fonts/DSEG14-Modern/DSEG14Modern-Bold.woff"
+  },
+  {
+    "revision": "40dc984227feecf9feb2f257d54a5e6d",
+    "url": "fonts/DSEG14-Modern/DSEG14Modern-Bold.woff2"
+  },
+  {
+    "revision": "8a9534a41de65458f7073ebaf29bc50d",
+    "url": "fonts/DSEG7-Modern/DSEG7Modern-Bold.woff"
+  },
+  {
+    "revision": "6bdbf8663a5747b0ece59fa24bfde055",
+    "url": "fonts/DSEG7-Modern/DSEG7Modern-Bold.woff2"
+  },
+  {
+    "revision": "26071d242d45e29cfb07c9e524395656",
+    "url": "images/akai-professional-1.svg"
+  },
+  {
+    "revision": "dd0aeff19c013db0037d9b5acf9cfbe0",
+    "url": "images/favicon/favicon-128x128.png"
+  },
+  {
+    "revision": "f3016201c777443229c146ce6ac70b88",
+    "url": "images/favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "fb003b01f037712da51d706c515f49e6",
+    "url": "images/favicon/favicon-32x32.png"
+  },
+  {
+    "revision": "4630e26e45a0f8c3fd97f3841ef69645",
+    "url": "images/favicon/favicon-64x64.png"
+  },
+  {
+    "revision": "490ea0dbcbf193d6d1f1a66eccaf4da0",
+    "url": "images/favicon_ico.png"
+  },
+  {
+    "revision": "c23806284d66e5eddf8971e99aa70f27",
+    "url": "images/pwa-icons/android-chrome-128x128.png"
+  },
+  {
+    "revision": "ef5cd3bba2d06a56311b938277a9fe70",
+    "url": "images/pwa-icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "1e57bd48d1c6abf0ddc81c0bc0f15b93",
+    "url": "images/pwa-icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "52ecf6a8729d44caba82d8bbdedec92d",
+    "url": "images/pwa-icons/android-chrome-72x72.png"
+  },
+  {
+    "revision": "f61a3afe3c6c5db76a9bb2a5b626905e",
+    "url": "images/pwa-icons/android-chrome-96x96.png"
+  },
+  {
+    "revision": "ef5cd3bba2d06a56311b938277a9fe70",
+    "url": "images/pwa-icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "1e57bd48d1c6abf0ddc81c0bc0f15b93",
+    "url": "images/pwa-icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "90100a77a007e8c854092e1e260df1c3",
+    "url": "images/webautio-ctrls/knobs/ANGEL_Sewage_62x62knob.png"
+  },
+  {
+    "revision": "f6ad66b2e99756507a48ed9defac9bab",
+    "url": "images/webautio-ctrls/knobs/JP8000.png"
+  },
+  {
+    "revision": "92235736680546de90ecdd5daa8f811d",
+    "url": "images/webautio-ctrls/knobs/Knob_18.png"
+  },
+  {
+    "revision": "caf6457e288db4ee4ecffd64e9150d74",
+    "url": "images/webautio-ctrls/knobs/LittlePhatty.png"
+  },
+  {
+    "revision": "f383aded2f8ab1a2f6067c29f881a1bf",
+    "url": "images/webautio-ctrls/knobs/knob_64_white1.png"
+  },
+  {
+    "revision": "962d6c12de6a8110f3d63bfb8717ce70",
+    "url": "images/webautio-ctrls/knobs/simplegray.png"
+  },
+  {
+    "revision": "cb818da4a70c8d120d5509fecdb1568b",
+    "url": "images/webautio-ctrls/sliders/PC3_EnvFader_03_Orange.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "5f56fe703cd061c0a620403724d448d5",
+    "url": "index.html"
+  },
+  {
+    "revision": "8ed487dc7ed4f46bae04",
+    "url": "js/app.11dd4c3a.js"
+  },
+  {
+    "revision": "df98d6f3521336f89215",
+    "url": "js/chunk-vendors.fd0ed48d.js"
+  },
+  {
+    "revision": "41267b1599a8589b5dff3225fc226488",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "37c7473a192206c2712e71378ee691ee",
+    "url": "scripts/3rdparty/webaudio-controls.js"
+  },
+  {
+    "revision": "616b2bcfec089d8d6250a03b2498b1ac",
+    "url": "styles/lds-ripple.css"
+  },
+  {
+    "revision": "532960b00283ab15edec84aa7e53e31b",
+    "url": "styles/main.css"
+  }
+]);
